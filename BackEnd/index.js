a4cbbbc1282c8744fe5617225ae65cf7758761ce
Aziz <ge65wol@mytum.de>
2023-08-14 19:37:16 +0200

    new api added, still need some development

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,34 +1,24 @@
-const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config()
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.getElementById("submit-button")
   const userInput = document.getElementById("userinput");
   const responseContainer = document.getElementById("response-container");
   
-  submitButton.addEventListener("click", function () {
+  submitButton.addEventListener("click", async function () {
+  const response = await fetch("http://localhost:3000/openai-completion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
 
-    const inputText = userInput.value;
-    responseContainer.textContent = runCompletion(inputText);
+    },
+    body: JSON.stringify({ userInput }),
+
+    })
+    const responsedata = await response.json();
+    responseContainer.textContent = responsedata.text;
   });
 });
 
 
-async function runCompletion (userInput) {
-    const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: userInput,
-    max_tokens:100
-    });
 
-    return completion.data.choices[0].text;
-}
 
